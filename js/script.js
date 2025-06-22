9f61e0a05f68810b837a4789a1e5c879a86ac236
ashlyhuamani <ashlyhuamaniatiquipa.@gmail.com>
2025-06-22 17:14:48 -0500

    js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Por ahora no hay interactividad avanzada,
+// pero puedes agregar funciones aquí si quieres.
+
+window.addEventListener('load', () => {
+    console.log('Portafolio cargado correctamente.');
+});
